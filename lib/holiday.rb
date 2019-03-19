16a7ed5de503e8cb762a4f79f9ecf78e2ee38241
Joe Tustin <j.tustin@gmail.com>
2019-03-19 18:39:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, supply|
 
 
-      
+
       if holiday.to_s.include?("_")
         holiday_string = String.new
         holiday.to_s.split("_").collect do |word|
