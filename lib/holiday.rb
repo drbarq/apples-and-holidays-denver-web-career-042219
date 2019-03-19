5a14fab2b801c47c80c24a7f280d4aa31949c449
Joe Tustin <j.tustin@gmail.com>
2019-03-19 17:01:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -30,6 +30,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
     if season == :winter
       if holiday == :christmas
         :christmas << supply
+      end 
+    end 
+  end
 
 end
 
