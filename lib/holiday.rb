88601a5291e747c5496991016f8c1f4adb200b67
Joe Tustin <j.tustin@gmail.com>
2019-03-19 19:47:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,10 +77,10 @@ def all_supplies_in_holidays(holiday_hash)
         holiday.to_s.split("_").collect do |word|
           holiday_array << word.capitalize!
         end
-        puts "#{holiday_array.join(" ")}: #{supply.flatten}"
+        puts "#{holiday_array.join(" ")}: #{supply.to_s}"
       ##  puts "#{supply}"
       else
-        puts "#{holiday.to_s.capitalize}: #{supply.flatten}"
+        puts "#{holiday.to_s.capitalize}: #{supply.to_s}"
 
       end
 
