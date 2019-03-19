ea028caf7b0389ccf292a63fefca43bed1873e2f
Joe Tustin <j.tustin@gmail.com>
2019-03-19 17:24:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,9 +80,9 @@ def all_supplies_in_holidays(holiday_hash)
     holiday.each do |holiday, supply|
       puts "#{holiday.to_s.capitalize}:"
       puts "#{supply.to_s.capitalize}"
-      supply.each do |supply|
-        puts supply.to_s
-      end
+    ##  supply.each do |supply|
+    ##    puts supply.to_s
+    ##  end
     end
   end
 
